63826a528efa8d5850f668be816f38c5d7097d48
Tmac4930 <32606108+Tmac4930@users.noreply.github.com>
2019-01-06 21:29:44 -0600

    Menu content added
    Menu assets, scenes and scripts added.
    Company name added

---
+++ Assets/Scripts/UI Scripts/BackgroundScroll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour {
+    [SerializeField]
+    private float scrollSpeed;
+
+    Vector3 startPOS;
+	// Use this for initialization
+	void Start ()
+    {
+        startPOS = transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate((new Vector3(-1, 0, 0)) * scrollSpeed * Time.deltaTime);
+
+        if(transform.position.x <-864.5)
+        {
+            transform.position = startPOS;
+        }
+	}
+}
---
+++ Assets/Scripts/UI Scripts/QuitScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitScene : MonoBehaviour {
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+        Debug.Log("Application Closed" );
+    }
+
+}
